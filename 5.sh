cf7ab266f6b95a77481ad3457d2e354053d5269c
nish72004 <152774728+nish72004@users.noreply.github.com>
2024-08-06 16:05:30 +0530

    Create 5.sh

---
+++ 5.sh
@@ -0,0 +1,12 @@
+echo "enter the file name"
+read file
+echo "enter the pattern"
+read pattern
+grep -o $pattern $file
+if [ $? -eq 0 ]; then
+
+        echo "patten  found"
+else
+        echo "pattern  not found"
+fi
+~    
