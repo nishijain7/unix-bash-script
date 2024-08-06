e1201b9f0f17dc63511a7024f456bf3c4f57ad3e
nish72004 <152774728+nish72004@users.noreply.github.com>
2024-08-06 19:58:11 +0530

    Create 8.sh

---
+++ 8.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+y=1
+while [ $y -eq 1 ]
+do
+        echo "enter item code"
+        read itemcode
+        echo "enter item"
+        read item
+        echo $itemcode $item >> item.txt
+       echo "enter 1 to contine or 0 to discontinue"
+       read y
+done
+cat item.txt
