017b792108006551b0962c7b2f1105481aa467cb
NISHI JIAN <nish72004@gmail.com>
2024-08-29 21:22:14 +0530

    Update setf.c

--- setjump.c
+++ setjump.c
@@ -1,33 +1,26 @@
-#include<stdio.h>
-#include<setjmp.h>
+#include <stdio.h>
+#include <setjmp.h>
 
 static jmp_buf envBuf;
 
-void second()
-{
-        printf("Second\n");
-        longjmp(envBuf,2);
+void second() {
+    printf("Second\n");
+    longjmp(envBuf, 2);
 }
 
-void first()
-{
-        second();
-        printf("First\n");
+void first() {
+    second();
+    printf("First\n");
 }
 
-int main(int argc,char** argv)
-{
-
-        int x = setjmp(envBuf);
-        //printf("%d \n",x);
-        if(x==0)
-        {
-                printf("x=%d, thus setjmp is called first time\n",x);
-                first();
-        }
-        else
-        {
-                printf("x=%d, setjmp is not called first time, thus returns the value specified in the longjmp \n",x);
-                printf("main\n");
-        }
+int main(int argc, char** argv) {
+    int x = setjmp(envBuf);
+    if (x == 0) {
+        printf("x=%d, thus setjmp is called first time\n", x);
+        first();
+    } else {
+        printf("x=%d, setjmp is not called first time, thus returns the value specified in the longjmp \n", x);
+        printf("main\n");
+    }
+    return 0;
 }
